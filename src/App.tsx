2fcf51ca64c74fe7c5eb1810e2aefb4b7f452a8c
Sattwik Sahu <sattwik21@iiserb.ac.in>
2023-05-05 21:48:47 +0530

    Configured TailwindCSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <section className="banner">
+        <div className="large-text">This page was made using</div>
+        <div className="gradient-text">
+          <span className="large-text" id="vite">
+            ViteJS
+          </span>
+          <span className="text-8xl flex items-center">+</span>
+          <span className="large-text" id="react">
+            React
+          </span>
+        </div>
+      </section>
+      <footer>
+        Made with <span className="text-red-600">&nbsp;&hearts;&nbsp;</span> on
+        VS Code.
+      </footer>
+    </div>
+  );
+};
 
-export default App
+export default App;
